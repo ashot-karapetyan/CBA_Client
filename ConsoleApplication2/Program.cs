de572449feaf5630109f5635b04fb9f023e9d16d
Ashot Karapetyan <karapetyan.ash@gmail.com>
2013-03-30 23:47:25 +0400

    initial commit for cba client.

---
+++ ConsoleApplication2/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            CBA.GateSoapClient  client = new  CBA.GateSoapClient ( );
+            client.Open();
+            CBA.ExchangeRates rates = client.ExchangeRatesByDate(DateTime.Today );
+            foreach (var rate in rates.Rates)
+            {
+                Console.WriteLine(rate.ISO + "  " + rate.Rate + " for " + rate.Amount); 
+            
+            }
+            Console.ReadLine();
+                
+   
+        }
+    }
+}
